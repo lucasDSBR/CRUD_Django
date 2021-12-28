56686c280faa0796a0d4decea2eda72ac6bcf8de
lucas <lucasmaciel6690@gmail.com>
2021-12-28 20:38:57 -0300

    Modificações

--- product/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-# Create your views here.
-def index(request):
-    if request.method == 'GET':
-        return HttpResponse("Metodo GET")
-    return HttpResponse(request)
--- shed/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Shed(models.Model):
-    cod = models.IntegerField(default=0)
-    capacity = models.FloatField(default=0)
-    def __str__(self):
-        return self.cod
